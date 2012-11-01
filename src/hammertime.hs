b463141f145789fce85df2c8bddc9614e75c8e2f
Julien Tanguy <julien.tanguy@jhome.fr>
2012-11-01 20:38:07 +0100

    Hlint corrections

--- src/hammertime.hs
+++ src/hammertime.hs
@@ -34,41 +34,40 @@ appendStop = fmap Stop getCurrentTime >>= appendEvent
 
 
 appendEvent :: Event -> IO ()
-appendEvent e = do
-    appendFile filename $ (show e ++ "\n")
+appendEvent e = appendFile filename (show e ++ "\n")
 
 showSavedEvents :: String -> IO ()
 showSavedEvents q = do
     d <- getCurrentTime
     cs <- readFile filename
-    mapM_ (putStrLn . show) $ readEvents cs
+    mapM_ print $ readEvents cs
     return ()
 
 readSavedEvents :: IO [Event]
 readSavedEvents = fmap readEvents $ readFile filename
 
 removeFirstStop :: [Event] -> [Event]
-removeFirstStop ((Stop _):t) = t
+removeFirstStop (Stop _:t) = t
 removeFirstStop events = events
 
 removeLastStart :: [Event] -> [Event]
 removeLastStart events = case reverse events of
-    ((Start _ _):t) -> reverse t
+    (Start _ _:t) -> reverse t
     events -> reverse events
 
 insertStops :: [Event] -> [Event]
 insertStops cs = reverse $ foldl step [] cs where
     step acc e@(Start _ time) = case acc of
-        ((Start _ _):_) -> e:(Stop time):acc -- A task was running, stop it now
+        (Start _ _:_) -> e: Stop time :acc -- A task was running, stop it now
         _ -> e:acc
     step acc e@(Stop _) = case acc of
-        ((Stop _):_) -> acc -- Can's stop a task twice, discard the last stop
+        (Stop _:_) -> acc -- Can's stop a task twice, discard the last stop
         _ -> e:acc
 
 
 computeTimes :: [Event] -> [Span]
-computeTimes cs = catMaybes . (map getDiff) . buildIntervals $ cs where
-    getDiff ((Start a begin), (Stop end)) = Just $ Span a begin end
+computeTimes cs = mapMaybe getDiff (buildIntervals cs) where
+    getDiff (Start a begin, Stop end) = Just $ Span a begin end
     getDiff _ = Nothing
     buildIntervals (x:y:t) = (x,y):buildIntervals t
     buildIntervals _ = []
@@ -84,13 +83,13 @@ getProjectTime p = getTotalTime . filterByActivity (\a -> p == project a)
 getActivityTime p = getTotalTime . filterByActivity (\a -> p == name a)
 
 filterByActivity :: (Activity -> Bool) -> [Span] -> [Span]
-filterByActivity p spans = filter p' $ spans where
+filterByActivity p = filter p' where
     p' = p . activity
 
 readEvents :: String -> [Event]
-readEvents s = catMaybes $ map readEvent (lines s)
+readEvents s = mapMaybe readEvent (lines s)
 
-readEvent line = listToMaybe . (map fst) . (take 1) $ reads line
+readEvent line = listToMaybe . map fst . take 1 $ reads line
 
 
 
