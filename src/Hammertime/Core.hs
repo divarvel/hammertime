1d7287267e413ae64708cc587c93936336836888
Julien Tanguy <julien.tanguy@jhome.fr>
2012-11-19 13:57:08 +0100

    Split hammertime into main, core and types modules

---
+++ src/Hammertime/Core.hs
@@ -0,0 +1,83 @@
+
+module Hammertime.Core where
+
+import System.IO
+import qualified Data.Text as T
+import Data.Time
+import Data.Time.Clock.POSIX
+import Data.List
+import Data.Maybe
+
+import Hammertime.Types
+
+filename = "hammertime.txt"
+
+
+createStart :: Activity -> IO Event
+createStart a = fmap (Start a) getCurrentTime
+
+appendStart :: Project -> Name -> [Tag] -> IO ()
+appendStart p n ts = createStart (Activity p n ts) >>= appendEvent
+
+appendStop :: IO ()
+appendStop = fmap Stop getCurrentTime >>= appendEvent
+
+
+appendEvent :: Event -> IO ()
+appendEvent e = appendFile filename (show e ++ "\n")
+
+showSavedEvents :: T.Text -> IO ()
+showSavedEvents _ = do
+    d <- getCurrentTime
+    cs <- readFile filename
+    mapM_ print $ readEvents . T.pack $ cs
+    return ()
+
+readSavedEvents :: IO [Event]
+readSavedEvents = fmap (readEvents . T.pack) $ readFile filename
+
+removeFirstStop :: [Event] -> [Event]
+removeFirstStop (Stop _:t) = t
+removeFirstStop events = events
+
+removeLastStart :: [Event] -> [Event]
+removeLastStart events = case reverse events of
+    (Start _ _:t) -> reverse t
+    events -> reverse events
+
+insertStops :: [Event] -> [Event]
+insertStops cs = reverse $ foldl step [] cs where
+    step acc e@(Start _ time) = case acc of
+        (Start _ _:_) -> e: Stop time :acc -- A task was running, stop it now
+        _ -> e:acc
+    step acc e@(Stop _) = case acc of
+        (Stop _:_) -> acc -- Can's stop a task twice, discard the last stop
+        _ -> e:acc
+
+
+computeTimes :: [Event] -> [Span]
+computeTimes cs = mapMaybe getDiff (buildIntervals cs) where
+    getDiff (Start a begin, Stop end) = Just $ Span a begin end
+    getDiff _ = Nothing
+    buildIntervals (x:y:t) = (x,y):buildIntervals t
+    buildIntervals _ = []
+
+getDiffTime :: Span -> NominalDiffTime
+getDiffTime (Span _ begin end) = diffUTCTime end begin
+
+getTotalTime :: [Span] -> NominalDiffTime
+getTotalTime spans = sum . map getDiffTime $ spans
+
+getProjectTime p = getTotalTime . filterByActivity ((==p) . project)
+
+getActivityTime p = getTotalTime . filterByActivity ((p==) . name)
+
+filterByActivity :: (Activity -> Bool) -> [Span] -> [Span]
+filterByActivity p = filter p' where
+    p' = p . activity
+
+readEvents :: T.Text -> [Event]
+readEvents s = mapMaybe readEvent (T.lines s)
+
+readEvent :: T.Text -> Maybe Event
+readEvent line = listToMaybe . map fst . take 1 . reads . T.unpack $ line
--- src/hammertime.hs
+++ src/hammertime.hs
@@ -1,102 +1,16 @@
-import System.IO
-import qualified Data.Text as T
-import Data.Time
-import Data.Time.Clock.POSIX
-import Data.List
-import Data.Maybe
+
+module Hammertime where
+
 import System.Environment
+import qualified Data.Text as T
 
-type Project = T.Text
-type Name = T.Text
-type Tag = T.Text
-
-data Activity = Activity { project :: Project
-                         , name :: Name
-                         , tags :: [Tag]
-                         } deriving (Eq, Read, Show)
-data Event = Start Activity UTCTime | Stop UTCTime deriving (Eq, Read, Show)
-data Span = Span { activity :: Activity
-                 , begin :: UTCTime
-                 , end :: UTCTime
-                 }
-
-filename = "hammertime.txt"
+import Hammertime.Core
 
 useArgs :: [String] -> IO ()
 useArgs ("show":q:_) = showSavedEvents $ T.pack q
 useArgs ("start":p:t:ts) = appendStart (T.pack p) (T.pack t) (map T.pack ts)
 useArgs ("stop":_) = appendStop
 useArgs _ = putStrLn "Not enough arguments"
-
-createStart :: Activity -> IO Event
-createStart a = fmap (Start a) getCurrentTime
-
-appendStart :: Project -> Name -> [Tag] -> IO ()
-appendStart p n ts = createStart (Activity p n ts) >>= appendEvent
-
-appendStop :: IO ()
-appendStop = fmap Stop getCurrentTime >>= appendEvent
-
-
-appendEvent :: Event -> IO ()
-appendEvent e = appendFile filename (show e ++ "\n")
-
-showSavedEvents :: T.Text -> IO ()
-showSavedEvents _ = do
-    d <- getCurrentTime
-    cs <- readFile filename
-    mapM_ print $ readEvents . T.pack $ cs
-    return ()
-
-readSavedEvents :: IO [Event]
-readSavedEvents = fmap (readEvents . T.pack) $ readFile filename
-
-removeFirstStop :: [Event] -> [Event]
-removeFirstStop (Stop _:t) = t
-removeFirstStop events = events
-
-removeLastStart :: [Event] -> [Event]
-removeLastStart events = case reverse events of
-    (Start _ _:t) -> reverse t
-    events -> reverse events
-
-insertStops :: [Event] -> [Event]
-insertStops cs = reverse $ foldl step [] cs where
-    step acc e@(Start _ time) = case acc of
-        (Start _ _:_) -> e: Stop time :acc -- A task was running, stop it now
-        _ -> e:acc
-    step acc e@(Stop _) = case acc of
-        (Stop _:_) -> acc -- Can's stop a task twice, discard the last stop
-        _ -> e:acc
-
-
-computeTimes :: [Event] -> [Span]
-computeTimes cs = mapMaybe getDiff (buildIntervals cs) where
-    getDiff (Start a begin, Stop end) = Just $ Span a begin end
-    getDiff _ = Nothing
-    buildIntervals (x:y:t) = (x,y):buildIntervals t
-    buildIntervals _ = []
-
-getDiffTime :: Span -> NominalDiffTime
-getDiffTime (Span _ begin end) = diffUTCTime end begin
-
-getTotalTime :: [Span] -> NominalDiffTime
-getTotalTime spans = sum . map getDiffTime $ spans
-
-getProjectTime p = getTotalTime . filterByActivity ((==p) . project)
-
-getActivityTime p = getTotalTime . filterByActivity ((p==) . name)
-
-filterByActivity :: (Activity -> Bool) -> [Span] -> [Span]
-filterByActivity p = filter p' where
-    p' = p . activity
-
-readEvents :: T.Text -> [Event]
-readEvents s = mapMaybe readEvent (T.lines s)
-
-readEvent :: T.Text -> Maybe Event
-readEvent line = listToMaybe . map fst . take 1 . reads . T.unpack $ line
-
 
 
 main = getArgs >>= useArgs
