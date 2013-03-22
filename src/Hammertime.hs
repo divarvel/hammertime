a46e46a786778b5df74a7a47821cad5367fdea9f
Julien Tanguy <julien.tanguy@jhome.fr>
2013-03-22 01:05:07 +0100

    Fix hunit tests

--- src/Hammertime.hs
+++ src/Hammertime.hs
@@ -1,7 +1,9 @@
 module Hammertime
     ( module Hammertime.Core
+    , module Hammertime.Storage
     , module Hammertime.Types
     ) where
 
 import Hammertime.Core
 import Hammertime.Types
+import Hammertime.Storage
