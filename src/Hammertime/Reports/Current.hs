cc793b693226408d1a30a4ab7a15aef1925c7b66
Clement Delafargue <clement@delafargue.name>
2013-03-28 09:47:54 +0100

    Fix: add missings line endings in current activity report

--- src/Hammertime/Reports/Current.hs
+++ src/Hammertime/Reports/Current.hs
@@ -11,10 +11,10 @@ import Hammertime.Types
 import Hammertime.Core
 
 report :: Maybe Span -> Report
-report = maybe "No current activity" mkReport
+report = maybe "No current activity\n" mkReport
     where
         mkReport s =
             (project . activity $ s) `mappend` ": " `mappend`
             (name . activity $ s) `mappend` " " `mappend`
-            (T.pack . show . roundedTime $ s) `mappend` "s"
+            (T.pack . show . roundedTime $ s) `mappend` "s" `mappend` "\n"
         roundedTime s = round . getDiffTime $ s :: Integer
