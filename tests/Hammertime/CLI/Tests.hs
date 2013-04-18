c2810a72075f3d0d661141809a5294347fd238a6
Clement Delafargue <clement@delafargue.name>
2013-04-18 23:02:25 +0200

    Tests for cliParserInfo

--- tests/Hammertime/CLI/Tests.hs
+++ tests/Hammertime/CLI/Tests.hs
@@ -1,9 +1,134 @@
 module Hammertime.CLI.Tests (tests) where
 
-import Test.Framework (Test, testGroup)
+import Test.Framework.Providers.HUnit
+import Test.Framework
+import Test.HUnit hiding (test, Test)
+
+import Options.Applicative
+
+import Hammertime.CLI (cliParserInfo, Action(..))
+import Hammertime.Types (TimeSpan(..), ReportType(..))
 
 tests :: Test
-tests = testGroup "Hakyll.Core.Util.String.Tests" $ concat
+tests = testGroup "Hakyll.Core.Util.String.Tests"
     [
+        testGroup "Start"
+        [
+            testCase "start" testStart,
+            testCase "start with tags" testStartWithTags,
+            testCase "bogus start" testBogusStart
 
+        ],
+        testCase "stop" testStop,
+        testGroup "Report"
+        [
+            testCase " default report" testReport,
+            testGroup "report with span"
+            [
+                testCase "month" $ testReportSpan ("month", Month),
+                testCase "week" $ testReportSpan ("week", Week),
+                testCase "day" $ testReportSpan ("day", Day)
+            ],
+            testGroup "report with filter"
+            [
+                testCase "project" testReportFilterProject,
+                testCase "activity" testReportFilterActivity,
+                testCase "tag" testReportFilterTag
+            ],
+            testGroup "report with type"
+            [
+                testCase "simple" $ testReportType ("simple", Simple),
+                testCase "total" $ testReportType ("total", TotalTime)
+            ]
+        ],
+        testCase "current" testCurrent
     ]
+
+
+runCliParser :: [String] -> Either ParserFailure Action
+runCliParser = execParserPure (prefs idm) cliParserInfo
+
+
+--------------------------------------------------------------------------------
+-- Start
+--
+
+testStart =
+    let result = runCliParser ["start", "project", "activity"]
+    in case result of
+        Left (ParserFailure _ _) -> assertFailure "parse fail: start"
+        Right a -> a @=? Start "project" "activity" []
+
+testStartWithTags =
+    let result = runCliParser ["start", "project", "activity", "tag1", "tag2", "tag3"]
+    in case result of
+        Left (ParserFailure _ _) -> assertFailure "parse fail: start with tags"
+        Right a -> a @=? Start "project" "activity" ["tag1", "tag2", "tag3"]
+
+testBogusStart =
+    let result = runCliParser ["start"]
+    in case result of
+        Left (ParserFailure m _) -> m "test" >> return ()
+        Right a -> assertFailure "bogus start accepted"
+
+--------------------------------------------------------------------------------
+-- Stop
+--
+
+testStop =
+    let result = runCliParser ["stop"]
+    in case result of
+        Left (ParserFailure _ _) -> assertFailure "parse fail: stop"
+        Right a -> a @=? Stop
+
+--------------------------------------------------------------------------------
+-- Report
+--
+
+testReport =
+    let result = runCliParser ["report"]
+    in case result of
+        Left (ParserFailure _ _) -> assertFailure "parse fail: report"
+        Right a -> a @=? (Report Day Nothing Nothing Nothing Simple)
+
+testReportSpan (string, span) =
+    let result = runCliParser ["report", string]
+    in case result of
+        Left (ParserFailure _ _) -> assertFailure "parse fail: report"
+        Right a -> a @=? (Report span Nothing Nothing Nothing Simple)
+
+testReportFilterProject =
+    let result = runCliParser ["report", "--project", "project"]
+    in case result of
+        Left (ParserFailure _ _) -> assertFailure "parse fail: report"
+        Right a -> a @=? (Report Day (Just "project") Nothing Nothing Simple)
+
+testReportFilterActivity =
+    let result = runCliParser ["report", "--activity", "activity"]
+    in case result of
+        Left (ParserFailure _ _) -> assertFailure "parse fail: report"
+        Right a -> a @=? (Report Day Nothing (Just "activity") Nothing Simple)
+
+testReportFilterTag =
+    let result = runCliParser ["report", "--tags", "tag"]
+    in case result of
+        Left (ParserFailure _ _) -> assertFailure "parse fail: report"
+        Right a -> a @=? (Report Day Nothing Nothing (Just "tag") Simple)
+
+testReportType (string, reportType) =
+    let result = runCliParser ["report", "-t", string]
+    in case result of
+        Left (ParserFailure _ _) -> assertFailure "parse fail: report"
+        Right a -> a @=? (Report Day Nothing Nothing Nothing reportType)
+
+
+--------------------------------------------------------------------------------
+-- Current
+--
+
+testCurrent =
+    let result = runCliParser ["current"]
+    in case result of
+        Left (ParserFailure _ _) -> assertFailure "parse fail: current"
+        Right a -> a @=? Current
+
