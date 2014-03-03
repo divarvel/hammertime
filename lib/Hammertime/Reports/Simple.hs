7b7484cb8d32931fc4e7d3036924f157d8e4ffc9
Clement Delafargue <clement@delafargue.name>
2014-03-03 14:15:11 +0100

    More readable times in Reports.Simple

--- lib/Hammertime/Reports/Simple.hs
+++ lib/Hammertime/Reports/Simple.hs
@@ -4,12 +4,14 @@ module Hammertime.Reports.Simple (
     report
 ) where
 
+import Data.Char (isDigit)
 import Data.Foldable (foldMap)
 import Data.Function (on)
 import Data.List (groupBy, sortBy)
 import Data.Maybe (mapMaybe)
 import Data.Monoid (mappend)
 import qualified Data.Text as T
+import Data.Time.Clock (NominalDiffTime)
 
 import Hammertime.Core (getTotalTime)
 import Hammertime.Types
@@ -24,13 +26,20 @@ report spans =
 header :: Report
 header = "Hammertime report \n\n"
 
+readableTime :: NominalDiffTime -> T.Text
+readableTime d =
+    let tsecs = (read . takeWhile isDigit . show $ d :: Int)
+        (hours, msecs) = divMod tsecs (3600)
+        (mins, secs) = divMod msecs (60)
+    in T.pack $ show hours ++ " hours " ++ show mins ++ " mins " ++ show secs ++ "s"
+
 reportProject :: (Project, [Span]) -> Report
 reportProject (p, spans) =
     let pairs = groupByActivity spans
         sorted = sortBy (compare `on` fst) pairs
         activities = foldMap reportActivity sorted
         totalTime = getTotalTime spans
-        displayedTime = T.pack $ show totalTime
+        displayedTime = readableTime totalTime
         firstLine = p `mappend` ": " `mappend`
                     displayedTime `mappend` "\n"
     in firstLine `mappend` activities `mappend` "\n"
@@ -38,7 +47,7 @@ reportProject (p, spans) =
 reportActivity :: (Name, [Span]) -> Report
 reportActivity (n, spans) =
     let totalTime = getTotalTime spans
-        displayedTime = T.pack $ show totalTime
+        displayedTime = readableTime totalTime
         tab = "  "
     in tab `mappend`
        n `mappend` ": " `mappend`
